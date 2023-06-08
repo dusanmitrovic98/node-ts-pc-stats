43f22666c0025e7969f9205f6b3cadc9c287c36c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-08 14:23:14 +0200

    Update pc-stats.ts

--- src/modules/pc-stats.ts
+++ src/modules/pc-stats.ts
@@ -2,3 +2,4 @@ import si, { Systeminformation } from "systeminformation";
 
 async function getSystemInfo(): Promise<Systeminformation.SystemData> {
   return await si.get({
+    system: "manufacturer, model, version",
