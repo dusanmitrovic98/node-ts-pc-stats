f701bb382e8893d55fc6938a7d303eba967be7e8
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-09 00:21:09 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
 import express, { Request, Response } from "express";
-import { Systeminformation } from "systeminformation";
-
-import getSystemInfo from "./modules/pc-stats.js";
-
-const app = express();
-const port = 3000;
-
-app.get("/", async (req: Request, res: Response) => {
-  try {
-    const data: Systeminformation.SystemData = await getSystemInfo();
-
-    res.json(data);
-  } catch (err: any) {
